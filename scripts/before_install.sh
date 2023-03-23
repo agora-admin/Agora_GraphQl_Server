a1fb88dbab6ceca1b3c30bdba9d5be21af6335a6
ryanjwong <ryanjwong007@gmail.com>
2023-03-22 22:01:32 -0700

    fix node install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #download node and npm
+sudo yum install -y gcc-c++ make 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+sudo rm -R /var/cache/yum/x86_64/2/nodesource/
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/agora-main"
