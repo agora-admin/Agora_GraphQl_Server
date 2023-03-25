32b5146be067920441a39622c05e5ee19394a73e
ryanjwong <ryanjwong007@gmail.com>
2023-03-25 14:22:09 -0700

    fixed slashes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -21,18 +21,18 @@ fi
 #import env
 cd /home/ec2-user/agora-main
 touch .env
-echo MONGODB=$(aws ssm get-parameter --region us-east-2 --name "MONGODB" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo SECRET_KEY=$(aws ssm get-parameter --region us-east-2 --name "SECRET_KEY" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo ADMIN_SERVER_URL=$(aws ssm get-parameter --region us-east-2 --name "ADMIN_SERVER_URL" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo HMS_APP_ACCESS_KEY=$(aws ssm get-parameter --region us-east-2 --name "HMS_APP_ACCESS_KEY" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo NODE_ENV=$(aws ssm get-parameter --region us-east-2 --name "NODE_ENV" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo HMS_DISCOURSE_TEMPLATE=$(aws ssm get-parameter --region us-east-2 --name "HMS_DISCOURSE_TEMPLATE" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo HMS_URI=$(aws ssm get-parameter --region us-east-2 --name "HMS_URI" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo ADMIN_SERVER_TOKEN=$(aws ssm get-parameter --region us-east-2 --name "ADMIN_SERVER_TOKEN" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo LP_KEY=$(aws ssm get-parameter --region us-east-2 --name "LP_KEY" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo LP_BASE_URL=$(aws ssm get-parameter --region us-east-2 --name "LP_BASE_URL" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo HMS_BEAM_URL=$(aws ssm get-parameter --region us-east-2 --name "HMS_BEAM_URL" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
-echo HMS_DOMAIN=$(aws ssm get-parameter --region us-east-2 --name "HMS_DOMAIN" --query "Parameter.Value" --output text) >> home/ec2-user/agora-main/.env
+echo MONGODB=$(aws ssm get-parameter --region us-east-2 --name "MONGODB" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo SECRET_KEY=$(aws ssm get-parameter --region us-east-2 --name "SECRET_KEY" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo ADMIN_SERVER_URL=$(aws ssm get-parameter --region us-east-2 --name "ADMIN_SERVER_URL" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo HMS_APP_ACCESS_KEY=$(aws ssm get-parameter --region us-east-2 --name "HMS_APP_ACCESS_KEY" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo NODE_ENV=$(aws ssm get-parameter --region us-east-2 --name "NODE_ENV" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo HMS_DISCOURSE_TEMPLATE=$(aws ssm get-parameter --region us-east-2 --name "HMS_DISCOURSE_TEMPLATE" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo HMS_URI=$(aws ssm get-parameter --region us-east-2 --name "HMS_URI" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo ADMIN_SERVER_TOKEN=$(aws ssm get-parameter --region us-east-2 --name "ADMIN_SERVER_TOKEN" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo LP_KEY=$(aws ssm get-parameter --region us-east-2 --name "LP_KEY" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo LP_BASE_URL=$(aws ssm get-parameter --region us-east-2 --name "LP_BASE_URL" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo HMS_BEAM_URL=$(aws ssm get-parameter --region us-east-2 --name "HMS_BEAM_URL" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
+echo HMS_DOMAIN=$(aws ssm get-parameter --region us-east-2 --name "HMS_DOMAIN" --query "Parameter.Value" --output text) >> /home/ec2-user/agora-main/.env
 
 
 
