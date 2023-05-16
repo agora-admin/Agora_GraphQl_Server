d514e69e55fe6e1d1cb0437f7cd54a9ec5aceaf7
ryanjwong <ryanjwong007@gmail.com>
2023-05-16 13:48:28 -0700

    changed port

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,7 +9,7 @@ sudo rm -R /var/cache/yum/x86_64/2/nodesource/
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
 
-#allow port 80 to run
+#allow port to run
 sudo yum install libcap2-bin 
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
